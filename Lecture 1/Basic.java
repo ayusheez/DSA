cb32c58f65904ce3d9ed8b6b97668ed7da45f07c
ayushi127 <gautamayushi912gmail.com>
2025-12-21 14:53:19 +0530

    Added Lecture 1 files

---
+++ Lecture 1/Basic.java
@@ -0,0 +1,10 @@
+
+public class Basic {
+    public static void main(String args[])
+    {
+        System.out.println("Coding Lecture 1");
+        System.out.println("Coding Lecture 100");
+        System.out.println("Soon 365 Days");
+    }
+      
+}
