67f9c1ef8393da6c62ab4ea34df0595c3cb04e7a
ayushi127 <gautamayushi912gmail.com>
2025-12-25 17:38:45 +0530

    Arrays in java

--- Lecture 1/small.java
+++ Lecture 1/small.java
@@ -52,29 +52,129 @@
 //     }
 // }
 //6. Add two numbers
-import java.util.Scanner;
+// import java.util.Scanner;
+// class small{
+//     public static void main(String args[])
+//     {
+//         int num1;
+//         int num2;
+//         System.out.println("Enter number 1:");
+//         Scanner obj1=new Scanner(System.in);
+//         num1=obj1.nextInt();
+
+//         System.out.println("Enter number 2:");
+//         Scanner obj2=new Scanner(System.in);
+//         num2=obj2.nextInt();
+        
+//         System.out.println("total:" +(num1+num2));
+        
+
+//     }
+// }
+
+//7. Even odd
+// import java.util.Scanner;
+// class small{
+//     public static void main(String args[]){
+//         System.out.print("Enter a number:");
+//         Scanner obj=new  Scanner(System.in);
+//         int num=obj.nextInt();
+//         if(num%2==0)
+//         System.out.println("Even number!");
+//         else
+//         System.out.println("Odd number!");
+//     }
+// }
+
+//8. check positive negative
+// import java.util.Scanner;
+// class small{
+//     public static void main(String args[]){
+//         System.out.println("Check If the number is positive.");
+//         System.out.println("Enter a number");
+//         Scanner obj=new Scanner(System.in);
+//         int num=obj.nextInt();
+//         if(num > 0)
+//         System.out.println("Positive number");
+//         else
+//         System.out.println("Negative number");
+
+//     }
+
+// }
+
+//9. for loop 1-5
+// import java.util.Scanner;
+// class small{
+//     public static void main(String args[]){
+//         System.out.println("Enter number:");
+//         Scanner obj=new Scanner(System.in);
+//         int n=obj.nextInt();
+//         for(int i=0;i<=n;i++)
+//         {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+//10. While loop
+// class small{
+//     public static void main(String args[])
+//     {
+//         System.out.println("While Loop");
+//         int i=0;
+//         while(i<=5)
+//         {
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+
+//11. Simple greet method
+// class small{
+    
+//     public static void main(String args[])
+//     {
+//      greet();
+//     }
+//     static void greet(){
+//         System.out.println("Ayushi hereeeee");
+//     }
+// }
+
+//13.Class and object
+// class small{
+//     String str="Ayushi";
+//     public static void main(String args[])
+//     {
+//         small  s=new small();
+//         System.out.println(s.str);
+        
+
+//     }
+// }
+
+//14. Simple array
 class small{
     public static void main(String args[])
     {
-        int num1;
-        int num2;
-        System.out.println("Enter number 1:");
-        Scanner obj1=new Scanner(System.in);
-        num1=obj1.nextInt();
+        System.out.println("Arrays in java");
+        int array[]={1,2,3,4,5};
+        System.out.println(array[0]);
+        System.out.println(array[4]);
 
-        System.out.println("Enter number 2:");
-        Scanner obj2=new Scanner(System.in);
-        num2=obj2.nextInt();
-        
-        System.out.println("total:" +(num1+num2));
-        
-
+        System.out.println("Different type of array");
+        int arr[]=new int[4];
+        arr[1]=9;
+        arr[2]=8;
+        arr[3]=7;
+   
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
     }
+
+
 }
 
-//7. Even odd
-//8. check positive negative
-//9. for loop 1-5
-//10. While loop
-//11. Simple greet method
-//13.Class and object
+
---
+++ Lecture 2/array.java
@@ -0,0 +1,103 @@
+
+//3.things- create 2 ways, modify, 
+import java.util.Scanner;
+
+public class array {
+    public static void main(String args[]) {
+        System.out.println("1D Array");
+        // 1.How to Declare and Initialize an array
+        int a[] = new int[3];
+        a[0] = 12;
+        a[1] = 123;
+        a[2] = 125;
+
+        // 2.How to declare + initialize an array
+        int b[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // 3.How to access Elements using array
+        // System.out.println(a[0]);
+        // System.out.println(b[0]);
+
+        // 4.How to change value of an array
+        // a[0]=2004;
+        // b[0]=2005;
+        // System.out.println(a[0]);
+        // System.out.println(b[0]);
+
+        // 5.How to Print All the Array
+        // for(int i=0;i<a.length;i++)
+        // System.out.println(a[i]);
+
+        // for(int i=0;i<b.length;i++)
+        // System.out.println(b[i]);
+
+        // 6.Print static arrays (integer)
+        // System.out.println(a[0]);
+        // System.out.println(a[1]);
+        // System.out.println(a[2]);
+
+        // 7.print static arrays(character)
+        // char ch[]={'a','b','c','d','e'};
+        // for(int i=0;i<ch.length;i++)
+        // System.out.println(ch[i]);
+
+        // 7.1 print array strings in java
+        // String str[]={"Advait","Agastya","Adhrit","Daiwik"};
+        // for(int i=0;i<str.length;i++)
+        // System.out.println(str[i]);
+
+        // 8.Print user Defined arrays-(numbera nd string)
+        // System.out.print("Enter size:");
+        // Scanner obj=new Scanner(System.in);
+        // int size=obj.nextInt();
+        // int array[]=new int[size];
+
+        // for(int i=0;i<array.length;i++)
+        // array[i]=obj.nextInt();
+
+        // for(int i=0;i<array.length;i++)
+
+        // System.out.print(array[i]);
+
+        // //Again For a String
+        // System.out.print("Enter size:");
+        // Scanner obj=new Scanner(System.in);
+        // int size=obj.nextInt();
+
+        // String str[]=new String[size];
+        // System.out.println("Enter male characters of kri's Marriage series");
+        // for(int i=0;i<str.length;i++)
+        // str[i]=obj.nextLine();
+
+        // for(int i=0;i<str.length;i++)
+        // System.out.println(str[i]);
+
+        // 9.Print Sum of all elements
+
+        // 10.Print maximum element
+        // 11.Reverse an Array
+        // 12.Check even or odd
+        // 13.Check if array numbers are palindrome or not?
+        // 14.Addition of Two Arrays
+        // int x[] = { 1, 2, 3 };
+        // int y[] = { 4, 5, 6 };
+        // int result[] = new int[x.length];
+        // if (x.length != y.length)
+        //     System.out.println("Length of x and y should be same");
+        // for (int i = 0; i < x.length; i++)
+        //     result[i] = x[i] + y[i];
+        // for (int i = 0; i < result.length; i++)
+        //     System.out.println(result[i]);
+
+      //10. printing 3D arrays
+
+      //11. Addition of 3D arrays
+    //   rahul,sunny,sonu,golu,gopichand = 78,98,97,12,45
+    
+      
+        
+
+
+    }
+
+}
