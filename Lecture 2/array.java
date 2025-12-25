05842c7ef66140eff602db1e07c55704722b4571
ayushi127 <ayushigautam1919@gmail.com>
2025-12-25 18:04:11 +0530

    Multidimensional arrays

--- Lecture 2/array.java
+++ Lecture 2/array.java
@@ -90,20 +90,35 @@ public class array {
         //     System.out.println(result[i]);
 
       //10. printing 3D arrays
-       int x[][]={{1,2,3},{4,5,6},{7,8,9}};
-       for(int i=0;i<x.length;i++){
-        for(int j=0;j<x[i].length;j++){
-            System.out.print(x[i][j]+ " ");
-        }
-        System.out.println();
-       }
+    //    int x[][]={{1,2,3},{4,5,6},{7,8,9}};
+    //    for(int i=0;i<x.length;i++){
+    //     for(int j=0;j<x[i].length;j++){
+    //         System.out.print(x[i][j]+ " ");
+    //     }
+    //     System.out.println();
+    //    }
      
 
 
       //11. Addition of 3D arrays
-    //    int x[][]={{1,2,3},{4,5,6},{7,8,9}};
-    //   int y[][]={{1,2,3},{4,5,6},{7,8,9}};
-    //   int result[][]=new int[x.length][y.length];
+      int x[][]={{1,2,3},{4,5,6},{7,8,9}};
+      int y[][]={{1,2,3},{4,5,6},{7,8,9}};
+      int result[][]=new int[x.length][y.length];
+      if(x.length!=y.length){
+        System.out.println("Size of X and Y should be same");
+      }
+      for(int i=0;i<x.length;i++){
+        for(int j=0;j<x[i].length;j++){
+            result[i][j]=x[i][j]+y[i][j];
+        }
+      }
+      for (int i=0;i<x.length;i++){
+        for(int j=0;j<x[i].length;j++){
+            System.out.print(result[i][j]+" ");
+        }
+        System.out.println();
+
+      }
     
     
       
