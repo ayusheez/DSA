178d135e78f9d127e308018c5cf7305f582b4db4
ayushi127 <ayushigautam1919@gmail.com>
2025-12-25 18:27:44 +0530

    maximum element

---
+++ Lecture 2/level2.java
@@ -0,0 +1,28 @@
+public class level2 {
+
+    public static void main(String args[])
+{
+// Find maximum element
+int arr[]={1,2,34,6,78,99,12,3,54544,2,1,};
+int max=arr[0];
+for(int i=1;i<arr.length;i++){
+    if(arr[i]>max){
+        max=arr[i];
+    }
+
+}
+System.out.print("Maximum element:"+max);
+
+// Find minimum element
+// Sum of all elements
+// Average of elements
+// Count even and odd numbers
+// Count positive, negative, zero
+// Reverse an array
+// Copy one array into another
+// Swap first and last element
+// Swap alternate elements
+
+}
+    
+}
