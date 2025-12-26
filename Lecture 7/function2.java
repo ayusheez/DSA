b40a4927cafe6cca06ef6175a93b47b59f850b6d
ayushi127 <ayushigautam1919@gmail.com>
2025-12-26 16:48:05 +0530

    sum

---
+++ Lecture 7/function2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class function2 {
+
+    public static void main(String args[]){
+    
+    }
+    //Function to print hello
+    void greet(){
+        System.out.println("HEllo Worldss");
+    }
+    // Function to add two numbers
+    void sum(){
+        int num1=90;
+        int num2=90;
+        int sum=num1+num2;
+        System.out.println(sum);
+    }
+    // Function to check even/odd
+    
+    // Function to find square of number
+   // Function to convert Celsius → Fahrenheit 
+    
+}
