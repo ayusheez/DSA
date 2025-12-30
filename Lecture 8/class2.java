9904dc787287d49a937952a7aae3ffe2cebd364e
ayushi127 <ayushigautam1919@gmail.com>
2025-12-30 17:58:23 +0530

    Burning Desire story

--- Lecture 8/class2.java
+++ Lecture 8/class2.java
@@ -1,26 +1,50 @@
 //1.class to show speed and brand- "Toyota is driving at 80km/h"
-class Car{
-    int speed;
-    String brand;
+// class Car{
+//     int speed;
+//     String brand;
 
-    void carInfo(){
-        System.out.println(brand + " is driving at "+ speed + "km/h");
-    }
+//     void carInfo(){
+//         System.out.println(brand + " is driving at "+ speed + "km/h");
+//     }
 
-}
-public class class2 {
-    public static void main(String args[]){
-        Car obj=new Car();
-        obj.speed=80;
-        obj.brand="Toyota";
+// }
+// public class class2 {
+//     public static void main(String args[]){
+//         Car obj=new Car();
+//         obj.speed=80;
+//         obj.brand="Toyota";
 
-        obj.carInfo();
+//         obj.carInfo();
 
 
-    }
+//     }
     
-}
+// }
+
+
 //2.Create a Book class with title and author Create 2 objects and print their details
+class Book{
+  String book;
+  String Author;
+
+  void bookList(){
+    System.out.println(book + " is written by " + Author);
+  }
+
+}
+public class class2{
+  public static void main(String args[]){
+    Book obj=new Book();
+    Book obj1=new Book();
+    obj.book="Marriage of Convenience";
+    obj.Author="RainaLove";
+    obj.bookList();
+    obj1.book="Burning Desire";
+    obj1.Author="unknown";
+    obj1.bookList();
+  }
+
+}
 //3.Create a BankAccount class
     //variables: accountNumber, balance
     //methods: deposit(), withdraw(), displayBalance()
