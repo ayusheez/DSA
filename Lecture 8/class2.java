a7c51f36d3c738d0675bc1ef7d767f15928a81b8
ayushi127 <ayushigautam1919@gmail.com>
2025-12-31 16:17:31 +0530

    failed hai bhaii

--- Lecture 8/class2.java
+++ Lecture 8/class2.java
@@ -49,49 +49,77 @@
 //3.Create a BankAccount class
     //variables: accountNumber, balance
     //methods: deposit(), withdraw(), displayBalance()
-    class bankAccount{
-       int accountNumber;
-       int balance;
+    // class bankAccount{
+    //    int accountNumber;
+    //    int balance;
        
 
-       void deposit(){
-        System.out.println("Money is deposited from the account:" + accountNumber);
+    //    void deposit(){
+    //     System.out.println("Money is deposited from the account:" + accountNumber);
 
-       }
-       void balance(){
-        System.out.println("Remaining balance:" + balance);
+    //    }
+    //    void balance(){
+    //     System.out.println("Remaining balance:" + balance);
 
-       }
+    //    }
 
-       void withdraw(){
-        System.out.println("Withdrawal Option is not Available");
+    //    void withdraw(){
+    //     System.out.println("Withdrawal Option is not Available");
 
-       }
-       void displayBalance(){
-        System.out.println("Balance is showed above");
+    //    }
+    //    void displayBalance(){
+    //     System.out.println("Balance is showed above");
 
-       }
+    //    }
 
-    }
-    class class2{
+    // }
+    // class class2{
 
-      public static void main(String args[]){
+    //   public static void main(String args[]){
 
-        bankAccount obj= new bankAccount();
-        obj.accountNumber=123456789;
-        obj.balance=69;
-        obj.deposit();
-        obj.balance();
-        obj.withdraw();
-        obj.displayBalance();
+    //     bankAccount obj= new bankAccount();
+    //     obj.accountNumber=123456789;
+    //     obj.balance=69;
+    //     obj.deposit();
+    //     obj.balance();
+    //     obj.withdraw();
+    //     obj.displayBalance();
 
 
-      }
+    //   }
 
-    }
+    // }
 //4.Create a Student class
     //name, marks
     //method to check pass/fail
+    class Student{
+      String  name;
+      int marks;
+
+      void check(){
+        if(marks>=45){
+          System.out.println(name + " is passed");
+        }
+        else{
+          System.out.println(name + " is failed");
+        }
+      }
+    }
+    class class2{
+      public static void main(String args[]){
+        Student obj=new Student();
+        Student obj1=new Student();
+        obj.name="Ayushi Gautam";
+        obj.marks=40;
+        obj1.name="Arshia Chandarki";
+        obj1.marks=90;
+
+        obj.check();
+        obj1.check();
+
+
+      }
+    }
 //5.create a Rectangle class
   // length, breadth
   // method to calculate area
