f0c6af7cfe28ac0aea61f69a5c41f14af06e5ea2
ayushi127 <ayushigautam1919@gmail.com>
2025-12-31 16:09:50 +0530

    bankkkk

--- Lecture 8/class2.java
+++ Lecture 8/class2.java
@@ -23,31 +23,72 @@
 
 
 //2.Create a Book class with title and author Create 2 objects and print their details
-class Book{
-  String book;
-  String Author;
+// class Book{
+//   String book;
+//   String Author;
 
-  void bookList(){
-    System.out.println(book + " is written by " + Author);
-  }
+//   void bookList(){
+//     System.out.println(book + " is written by " + Author);
+//   }
 
-}
-public class class2{
-  public static void main(String args[]){
-    Book obj=new Book();
-    Book obj1=new Book();
-    obj.book="Marriage of Convenience";
-    obj.Author="RainaLove";
-    obj.bookList();
-    obj1.book="Burning Desire";
-    obj1.Author="unknown";
-    obj1.bookList();
-  }
+// }
+// public class class2{
+//   public static void main(String args[]){
+//     Book obj=new Book();
+//     Book obj1=new Book();
+//     obj.book="Marriage of Convenience";
+//     obj.Author="RainaLove";
+//     obj.bookList();
+//     obj1.book="Burning Desire";
+//     obj1.Author="unknown";
+//     obj1.bookList();
+//   }
 
-}
+// }
+
 //3.Create a BankAccount class
     //variables: accountNumber, balance
     //methods: deposit(), withdraw(), displayBalance()
+    class bankAccount{
+       int accountNumber;
+       int balance;
+       
+
+       void deposit(){
+        System.out.println("Money is deposited from the account:" + accountNumber);
+
+       }
+       void balance(){
+        System.out.println("Remaining balance:" + balance);
+
+       }
+
+       void withdraw(){
+        System.out.println("Withdrawal Option is not Available");
+
+       }
+       void displayBalance(){
+        System.out.println("Balance is showed above");
+
+       }
+
+    }
+    class class2{
+
+      public static void main(String args[]){
+
+        bankAccount obj= new bankAccount();
+        obj.accountNumber=123456789;
+        obj.balance=69;
+        obj.deposit();
+        obj.balance();
+        obj.withdraw();
+        obj.displayBalance();
+
+
+      }
+
+    }
 //4.Create a Student class
     //name, marks
     //method to check pass/fail
