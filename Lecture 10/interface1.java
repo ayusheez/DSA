305ee3795c5893952fcfc2ddc131537f0126a1aa
ayushi127 <ayushigautam1919@gmail.com>
2026-01-03 23:55:08 +0530

    org

--- Lecture 10/interface1.java
+++ Lecture 10/interface1.java
@@ -2,6 +2,7 @@ public class interface1 {
     public static void main(String args[]){
         System.out.println("Again hello ;(/n");
         System.out.println("HashMap");
+        System.out.println("Mistakee");
     }
     
 }
